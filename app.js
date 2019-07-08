2b896cb60d07c4e07b12709b13ed7b1753d85942
LeszekSadkowski <leszek.sadkowski@gmail.com>
2019-07-08 20:16:24 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const memoryGame = {
+    tileCount:20,//number of bricks
+    tileOnRow:5,//number bricks on row
+    divBoard: null,//div with game board
+    divScore: null,//div with game score
+    tiles:[],//here comes randomized table
+    tilesChecked:[],//selected blocks
+    moveCount:0,//number of moves
+    tilesIMG:[// graph bricks
+        'title_1.png',
+        'title_2.png',
+        'title_3.png',
+        'title_4.png',
+        'title_5.png',
+        'title_6.png',
+        'title_7.png',
+        'title_8.png',
+        'title_9.png',
+        'title_1.png'
+    ]
+
+};
+
